920327c02c1ad80fdf7e4cd43e3b2bfaec6bae1e
kingdz618 <jindongzejava@163.com>
2017-06-26 05:31:34 +0800

    modify the download number

--- test/DownTingWa.java
+++ test/DownTingWa.java
@@ -11,109 +11,119 @@ import org.apache.commons.httpclient.params.HttpMethodParams;
 
 public class DownTingWa {
 
-    public static void main(String[] args) {
-        int start = 2565;
-        int mark = 0;
-        String baseUrl = "http://www.itingwa.com/listen/";
+	public static void main(String[] args) {
+		int start = 18161;
+		int mark = 0;
+		String baseUrl = "http://www.itingwa.com/listen/";
 
-        while (true) {
-            {
-                {
-                    System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
-                    System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
-                    System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.commons.httpclient",
-                            "error");
-                }
+		while (true) {
+			{
+				{
+					System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.SimpleLog");
+					System.setProperty("org.apache.commons.logging.simplelog.showdatetime", "true");
+					System.setProperty("org.apache.commons.logging.simplelog.log.org.apache.commons.httpclient", "error");
+				}
 
-                String songUrl = baseUrl + start;
+				String songUrl = baseUrl + start;
 
-                String htmlSource = null;
-                try {
-                    htmlSource = get(songUrl);
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-                if (htmlSource == null) {
-                    continue;
-                }
-                // System.out.println(htmlSource);
-                if (htmlSource.indexOf("页面未找到") != -1) {
-                    start++;
-                    continue;
-                }
-                int a = htmlSource.indexOf("frame1");
-                a = htmlSource.indexOf("<h1>", a) + 4;
-                int b = htmlSource.indexOf("<a href", a);
-                b = htmlSource.indexOf("<a href", a);
-                String fileName = "" + start + " - " + htmlSource.substring(a, b).trim();
-                fileName = HomeUtils.processName(fileName);
-                System.out.println(fileName);
+				String htmlSource = null;
+				try {
+					htmlSource = get(songUrl);
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				if (htmlSource == null) {
+					continue;
+				}
+				// System.out.println(htmlSource);
+				if (htmlSource.indexOf("页面未找到") != -1) {
+					System.out.println(start + "\t404");
+					start++;
+					continue;
+				}
+				int a = htmlSource.indexOf("frame1");
+				a = htmlSource.indexOf("<h1>", a) + 4;
+				int b = htmlSource.indexOf("<a href", a);
+				b = htmlSource.indexOf("<a href", a);
+				String fileName = null;
+				try {
+					fileName = "" + start + " - " + htmlSource.substring(a, b).trim();
+				} catch (Exception e1) {
+					try {
+						TimeUnit.SECONDS.sleep(5);
+					} catch (InterruptedException e) {
 
-                a = htmlSource.indexOf("<div id=\"tw_player\"", b);
-                a = htmlSource.indexOf("http", a);
-                b = htmlSource.indexOf("</div>", a) - 2;
-                String downloadUrl = htmlSource.substring(a, b);
-                System.out.println(downloadUrl);
+					}
+					continue;
+				}
+				fileName = HomeUtils.processName(fileName);
+				System.out.println(fileName);
 
-                File baseFilePath = new File("E:\\itingwa\\");
-                Html html = new Html();
-                String result = html.downloadMusicAtHome(downloadUrl, fileName, start, baseFilePath, null);
-                System.out.println(result);
-                if (result.contains("success")) {
-                    mark = 0;
-                    start++;
-                } else if (result.contains("already exists")) {
-                    start++;
-                } else if (result.contains("file length error")) {
-                    mark++;
-                    try {
-                        TimeUnit.SECONDS.sleep(10);
-                    } catch (InterruptedException e) {
+				a = htmlSource.indexOf("<div id=\"tw_player\"", b);
+				a = htmlSource.indexOf("http", a);
+				b = htmlSource.indexOf("</div>", a) - 2;
+				String downloadUrl = htmlSource.substring(a, b);
+				System.out.println(downloadUrl);
 
-                    }
-                    if (mark == 5) {
-                        start++;
-                        mark = 0;
-                    }
-                }
-            }
-        }
-    }
+				File baseFilePath = new File("D:\\gittest\\music\\");
+				Html html = new Html();
+				String result = html.downloadMusicAtHome(downloadUrl, fileName, start, baseFilePath, null);
+				System.out.println(result);
+				if (result.contains("success")) {
+					mark = 0;
+					start++;
+				} else if (result.contains("already exists")) {
+					start++;
+				} else if (result.contains("file length error")) {
+					mark++;
+					try {
+						TimeUnit.SECONDS.sleep(10);
+					} catch (InterruptedException e) {
 
-    public static String get(String url) throws Exception {
-        HttpClient httpClient = new HttpClient();
-        // 创建GET方法的实例
-        GetMethod getMethod;
-        try {
-            getMethod = new GetMethod(url);
-        } catch (Exception e1) {
-            throw e1;
-        }
-        // 使用系统提供的默认的恢复策略
-        getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
-        try {
-            // 执行getMethod
-            int statusCode = httpClient.executeMethod(getMethod);
-            if (statusCode != HttpStatus.SC_OK) {
-                System.err.println("Method failed: " + getMethod.getStatusLine());
-            }
-            // 读取内容
-            byte[] responseBody = getMethod.getResponseBody();
-            // 处理内容
-            // System.out.println(new String(responseBody));
-            return new String(responseBody, "utf-8");
-        } catch (HttpException e) {
-            // 发生致命的异常，可能是协议不对或者返回的内容有问题
-            System.out.println("Please check your provided http address!");
-            // e.printStackTrace();
-            throw e;
-        } catch (IOException e) {
-            // 发生网络异常
-            // e.printStackTrace();
-            throw e;
-        } finally {
-            // 释放连接
-            getMethod.releaseConnection();
-        }
-    }
+					}
+					if (mark == 5) {
+						start++;
+						mark = 0;
+					}
+				}
+			}
+		}
+	}
+
+	public static String get(String url) throws Exception {
+		HttpClient httpClient = new HttpClient();
+		// 创建GET方法的实例
+		GetMethod getMethod;
+		try {
+			getMethod = new GetMethod(url);
+		} catch (Exception e1) {
+			throw e1;
+		}
+		// 使用系统提供的默认的恢复策略
+		getMethod.getParams().setParameter(HttpMethodParams.RETRY_HANDLER, new DefaultHttpMethodRetryHandler());
+		try {
+			// 执行getMethod
+			int statusCode = httpClient.executeMethod(getMethod);
+			if (statusCode != HttpStatus.SC_OK) {
+				System.err.println("Method failed: " + getMethod.getStatusLine());
+			}
+			// 读取内容
+			byte[] responseBody = getMethod.getResponseBody();
+			// 处理内容
+			// System.out.println(new String(responseBody));
+			return new String(responseBody, "utf-8");
+		} catch (HttpException e) {
+			// 发生致命的异常，可能是协议不对或者返回的内容有问题
+			System.out.println("Please check your provided http address!");
+			// e.printStackTrace();
+			throw e;
+		} catch (IOException e) {
+			// 发生网络异常
+			// e.printStackTrace();
+			throw e;
+		} finally {
+			// 释放连接
+			getMethod.releaseConnection();
+		}
+	}
 }
