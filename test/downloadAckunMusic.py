073121baee43816629400290c38aa4bc250c2dc5
kingdz <jindongzejava@163.com>
2017-07-11 08:53:09 +0800

    add new logic

--- test/downloadAckunMusic.py
+++ test/downloadAckunMusic.py
@@ -9,6 +9,7 @@ import urllib
 import time
 import random
 
+# 获得响应头信息中的Content-Type域
 def urlOpenGetHeaders(url):
     req = urllib.request.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
@@ -16,6 +17,7 @@ def urlOpenGetHeaders(url):
     html = page.getheader('Content-Type')
     return html
 
+# 获得url的源码
 def urlOpen(url):
     req = urllib.request.Request(url)
     req.add_header('User-Agent', 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36')
@@ -34,6 +36,7 @@ def urlOpen(url):
     html = page.read()
     return html
 
+# 根据名字、后缀和url下载到本地文件夹
 def download(title,post,url):
     filename = title + "." +post
     with open(filename, 'wb') as f:
@@ -46,6 +49,7 @@ def getPostStr(pKey, song_id):
     encry = cipher.encrypt(song_id)
     return base64.b64encode(encry)
 
+# 获取歌曲的实际的url
 def getSongRealUrl(songidVal):
     url = 'http://www.aekun.com/api/getMusicbyid/'
     r = requests.post(url, {
@@ -53,22 +57,36 @@ def getSongRealUrl(songidVal):
     })
     return r.content
 
+# 将需要的数据写入本地文件
 def writeStrToFile(writeStr):
     print(writeStr)
     with open("downurl.txt","a",encoding="UTF-8") as f:
         f.write(writeStr)
         f.write("\n")
 
+# 获取最新的推荐歌曲编号
+def getMaxSongs():
+    url = "http://www.aekun.com/new/"
+    html = urlOpen(url).decode('utf-8')
+    a = html.find('<tr musicid=') + 13
+    b = html.find('"',a)
+    result = int(html[a:b])
+    return result
+
+# 下载歌曲的主程序部分
 def downloadMusicMain():
     # 获取pKey
     f = open('public.pem')
     pKey = f.read()
     f.close()
 
-    songIdInt = 3507101
-# 3505251			|10			|2015084685			|▌▌Chillout ▌▌Losing Ground Michael FK & Groundfold  -----3505251.mp3
+    songIdInt = 3509726
+    songIdInt = songIdInt + 1
+    maxSong = getMaxSongs()
+    print("start from:%s,end with:%s"%(songIdInt,maxSong))
+    # 3505251			|10			|2015084685			|▌▌Chillout ▌▌Losing Ground Michael FK & Groundfold  -----3505251.mp3
     while(True):
-        if songIdInt > 3509585:
+        if songIdInt > maxSong:
             break
         
         time.sleep(10)
@@ -77,16 +95,16 @@ def downloadMusicMain():
         except ConnectionResetError:
             print("Error occur")
         songId = str(songIdInt).encode('utf-8')
-        songIdInt = songIdInt + 1
         print(songId)
         songidVal = getPostStr(pKey, songId)
         
         try:
             ret = getSongRealUrl(songidVal)
-        except ConnectionError:
+        except (ConnectionError , ConnectionResetError):
             print("ConnectionError")
+            time.sleep(3)
             continue
-        
+
         ret = ret.decode('utf-8')
         #ret = '{"state":"success","message":"ok","action":null,"data":{"url":"http://us.aekun.com/upload/75AAB77BC2D16123F9F2E8B6C68FCB8E.mp3","song_name":"就算遇到挫折、受到嘲笑，也要勇敢的向前跑！","coll":0,"singername":"小哥","singerpic":"https://m4.aekun.com/user_l_5973822_20170513135220.png"}}'
         print(ret)
@@ -100,32 +118,28 @@ def downloadMusicMain():
         if isinstance(downUrl,str):
             if downUrl.strip() == '':
                 html = urlOpen("http://www.aekun.com/song/" + str(songIdInt)).decode('utf-8')
+                songIdInt = songIdInt + 1
                 continue
         elif isinstance(downUrl,dict):
             pass
         else:
             continue
 
-        #print("[",downUrl,"]")
         downUrl = ret['data']['url']
         if downUrl is None:
             continue
         if downUrl.strip() == "":
             continue
-        #print(downUrl)
         post = downUrl[-3:]
         post = post.lower()
         if post != 'mp3' and post != 'm4a':
             tmp = urlOpenGetHeaders(downUrl)
             if tmp.find('mp3') != -1:
                 post = 'mp3'
-        #print(post)
         songName = ret['data']['song_name']
-        #print(songName)
-        #download(songName,post,downUrl)
-        #print("download ok")
-        writeStr = "%-10s|%-50s|%-5s|%s"%((songIdInt - 1),songName,post,downUrl)
+        writeStr = "%-10s|%-50s|%-5s|%s"%(songIdInt,songName,post,downUrl)
         writeStrToFile(writeStr)
+        songIdInt = songIdInt + 1
 
 if __name__ == '__main__':
     downloadMusicMain()
